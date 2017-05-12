c272abe6f7dff08556d8b929e0ce681601648050
dondaum <sebastian.daum89@gmail.com>
2017-05-12 21:24:21 +0200

    created user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   #get "texts/counter" => "texts#counter"
   root 'texts#index'
   post 'counter' => 'texts#counter', as: :counter
   resources :texts
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428154824) do
+ActiveRecord::Schema.define(version: 20170507160537) do
 
   create_table "texts", force: :cascade do |t|
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "encrypted_password"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
 end
